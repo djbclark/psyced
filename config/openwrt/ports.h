5edef9e9576b3c0e9dcc05da9cba2aafd82e9955
psyc://psyced.org/~lynX <@>
2011-12-03 23:28:44 +0100

    default ports.h for openwrt unless admin changes the config

---
+++ config/openwrt/ports.h
@@ -0,0 +1,42 @@
+// CAUTION! This file has been generated using ../bin/psyconf.
+// Don't edit unless you are no longer going to use the configuration tool.
+// Edit psyced.ini instead, then run ../bin/psyconf again.
+
+/* the values in here may be the same as in services.h
+ * or they may be not. so always be aware which one's you
+ * are using to which purpose.	-lynX
+ */
+#ifndef PORTS_H
+#define PORTS_H
+
+#define	PSYC_PORT	4404
+#define	PSYCS_PORT	9404
+
+#define	HTTP_PORT	33333
+#define	HTTPS_PORT	34443
+
+#define	IRC_PORT	6667
+#define	IRCS_PORT	9999
+
+#define	JABBER_PORT	5222
+#define	JABBERS_PORT	5223
+#define	JABBER_S2S_PORT	5269
+
+#define	TELNET_PORT	2323
+#define	TELNETS_PORT	9992
+
+#define	APPLET_PORT	2008
+
+#define	POP3_PORT	
+#define	POP3S_PORT	
+
+#define	SMTP_PORT	2525
+#define	SMTPS_PORT	
+
+#define	NTTP_PORT	
+#define	NNTPS_PORT	
+
+// experimental PSYC 1.0 implementation
+#define	SPYC_PORT	
+
+#endif
